7a81b5efd7da72625a41207797e1aa420fcd40c8
VIRATI AKIRANANDHAN REDDY <viratiaki53@gmail.com>
2026-03-10 17:37:17 +0530

    chore: Add copyright notice

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+// © 2026 VIRATI AKIRANANDHAN REDDY
+// https://github.com/ViratiAkiraNandhanReddy/404-retro-window
+
 function changeColor(color) {
   document.body.style.backgroundColor = color;
   document.documentElement.style.setProperty('--bg-color', color);
